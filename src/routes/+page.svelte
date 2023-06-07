d913f2fd8296f39342518172e09c36deb9880ce5
LeaHaferbier <lea.haferbier@hfg-gmuend.de>
2023-06-07 15:44:47 +0200

    Filmdetails

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,19 @@
 <script>
-	import dummydata from '$lib/dummydata.js';
 	import MovieCard from '$lib/components/MovieCard.svelte';
 
-	let movies = dummydata;
+	export let data;
+	let movies = data.results;
 	console.log(movies);
 </script>
 
 <h1>Popular Movies</h1>
 
 <div class="movie-grid">
-	{#each movies as movieData}
-		<MovieCard movie={movieData} />
-	{/each}
+	{#if movies}
+		{#each movies as movieData}
+			<MovieCard movie={movieData} />
+		{/each}
+	{/if}
 </div>
 
 <style>
---
+++ src/routes/movie/[id]/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let data;
+	let id = data.id;
+
+	let movie = data;
+</script>
+
+<a href="/">zurück zur Filmübersicht</a>
+
+<h1>{movie.title}</h1>
+<p>{movie.release_date}</p>
+
+<style>
+	a {
+		color: red;
+	}
+</style>
